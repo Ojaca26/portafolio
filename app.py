5523709f101ec4624330da499b308d327938db22
Oscar J Carabali P <130184440+Ojaca26@users.noreply.github.com>
2025-11-04 14:36:25 -0500

    app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ with col_iana:
                 f"""
                 <a href='https://iana-datainsightsco.com/' target='_blank' style='text-decoration: none;'>
                     <div style='display: flex; justify-content: center; align-items: center; min-height: 200px;'>
-                        <img src='data:image/png;base64,{img_iana_b64}' width='450' style='max-width: 100%; height: auto; display: block;'>
+                        <img src='data:image/png;base64,{img_iana_b64}' width='550' style='max-width: 100%; height: auto; display: block;'>
                     </div>
                 </a>
                 """,
@@ -204,6 +204,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 
 
 
